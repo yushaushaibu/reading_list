16f8bd71451732fb6592be4156a1922f41ca4b42
yushaushaibu <yushaushaibu1@gmail.com>
2023-03-11 14:36:16 +0000

    Passing up props from child to parent component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import { useState } from "react";
+import BookCreate from "./components/BookCreate";
 
 function App() {
   const [books, setBooks] = useState([]);
-  return <div>App</div>;
+
+  const createBook = (title) => {
+    console.log("Add book with title: ", title);
+  };
+
+  return (
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,5 +1,27 @@
-function BookCreate() {
-  return <div>Book Create</div>;
+import { useState } from "react";
+
+function BookCreate({ onCreate }) {
+  const [title, setTitle] = useState("");
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onCreate(title);
+    setTitle("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Add a book</label>
+        <input value={title} onChange={handleChange} />
+        <button>Create</button>
+      </form>
+    </div>
+  );
 }
 
 export default BookCreate;
