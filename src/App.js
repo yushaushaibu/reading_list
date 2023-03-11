800d6883fe0a29f3d20d70472f2a551080d2a3a3
yushaushaibu <yushaushaibu1@gmail.com>
2023-03-11 20:12:26 +0000

    Passing down books prop form app.js to BookShow component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    const updatedBooks = [...books, { id: Math.floor(Math.random() * 999), 
-        title }];
+    const updatedBooks = [
+      ...books,
+      { id: Math.floor(Math.random() * 999), title },
+    ];
     setBooks(updatedBooks);
   };
 
   return (
-    <div>
-      {books.length}
+    <div className="app">
+      <BookList books={books} />
       <BookCreate onCreate={createBook} />
     </div>
   );
