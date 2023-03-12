07d965cc8caf2b6bd577edcc948c81e8c89b4943
yushaushaibu <yushaushaibu1@gmail.com>
2023-03-12 22:39:52 +0000

    Implemented BookEdit component to show form for editing book title

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,5 +1,25 @@
-function BookEdit() {
-  return <div>Book Edit</div>;
+import { useState } from "react";
+
+function BookEdit({ book }) {
+  const [title, setTitle] = useState(book.title);
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log("New title is:", title);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Title</label>
+      <input className="input" value={title} onChange={handleChange} />
+      <button className="button is-primary">Save</button>
+    </form>
+  );
 }
 
 export default BookEdit;
