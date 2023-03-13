b878464ac88b55afc487dac4f9e8efaa64ef707e
yushaushaibu <yushaushaibu1@gmail.com>
2023-03-13 16:40:45 +0000

    Adding Images to BookShow component

--- src/App.js
+++ src/App.js
@@ -33,9 +33,9 @@ function App() {
 
   return (
     <div className="app">
+      <h1>Reading List</h1>
       <BookList books={books} onEdit={editBookById} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
-      <div>Number of Books: {books.length}</div>
     </div>
   );
 }
--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -24,6 +24,7 @@ function BookShow({ book, onEdit, onDelete }) {
 
   return (
     <div className="book-show">
+      <img alt="books" src={`https://picsum.photos/seed/${book.id}/300/200`} />
       <div>{content}</div>
       <div className="actions">
         <button className="edit" onClick={handleEditClick}>
