db468582df9a6ec333f4d45a2f07b0f6493f5807
yushaushaibu <yushaushaibu1@gmail.com>
2023-03-14 13:18:04 +0000

    Refractoring entire codebase to use context system

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,22 +3,13 @@ import BooksContext from "../context/books";
 import BookShow from "./BookShow";
 
 function BookList({ books, onEdit, onDelete }) {
-  const { count, incrementCount } = useContext(BooksContext);
-
   const renderedBooks = books.map((book) => {
     return (
       <BookShow key={book.id} book={book} onEdit={onEdit} onDelete={onDelete} />
     );
   });
 
-  return (
-    <div className="book-list">
-      {count}
-      <button onClick={incrementCount}>Click</button>
-
-      {renderedBooks}
-    </div>
-  );
+  return <div className="book-list">{renderedBooks}</div>;
 }
 
 export default BookList;
