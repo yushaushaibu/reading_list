67f1ed0b05f51ed1d5c74e54beb3875957148f6e
yushaushaibu <yushaushaibu1@gmail.com>
2023-03-14 12:07:20 +0000

    Implemented context communication in project

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,22 @@
+import { useContext } from "react";
+import BooksContext from "../context/books";
 import BookShow from "./BookShow";
 
 function BookList({ books, onEdit, onDelete }) {
+  const num = useContext(BooksContext);
+
   const renderedBooks = books.map((book) => {
     return (
       <BookShow key={book.id} book={book} onEdit={onEdit} onDelete={onDelete} />
     );
   });
 
-  return <div className="book-list">{renderedBooks}</div>;
+  return (
+    <div className="book-list">
+      {num}
+      {renderedBooks}
+    </div>
+  );
 }
 
 export default BookList;
