ca2f28b7fd921a5536f4a618801ad5a5264cc7ae
yushaushaibu <yushaushaibu1@gmail.com>
2023-03-14 20:58:17 +0000

    BookList component is configured to reach into the BooksContext to retrieve books

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,11 +2,10 @@ import { useContext } from "react";
 import BooksContext from "../context/books";
 import BookShow from "./BookShow";
 
-function BookList({ books, onEdit, onDelete }) {
+function BookList() {
+  const { books } = useContext(BooksContext);
   const renderedBooks = books.map((book) => {
-    return (
-      <BookShow key={book.id} book={book} onEdit={onEdit} onDelete={onDelete} />
-    );
+    return <BookShow key={book.id} book={book} />;
   });
 
   return <div className="book-list">{renderedBooks}</div>;
